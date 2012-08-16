5ceb888c9b166561f773c8a9fe3ceee27e88e43a
Dave Schaefer <dave.schaefer@gmail.com>
2012-08-16 00:56:18 -0700

    cli - Add some error handling to 'build'

--- lib/vulcan/cli.rb
+++ lib/vulcan/cli.rb
@@ -39,14 +39,21 @@ if no COMMAND is specified, a sensible default will be chosen for you
     deps    = options[:deps]    || []
     server  = URI.parse(ENV["VULCAN_HOST"] || "http://#{app}.herokuapp.com")
 
-    source_is_url = URI.parse(source).scheme
+    begin
+      source_is_url = URI.parse(source).scheme
+    rescue URI::InvalidURIError
+      source_is_url = false
+    end
 
     Dir.mktmpdir do |dir|
       unless source_is_url
         input_tgz = "#{dir}/input.tgz"
         if File.directory?(source)
           action "Packaging local directory" do
-            %x{ cd #{source} && tar czvf #{input_tgz} . 2>&1 }
+            output = %x{ cd #{source} && tar czvf #{input_tgz} . 2>&1 }
+          end
+          if not File.exists?(input_tgz)
+            error "Could not create package: '#{output}'"
           end
         else
           input_tgz = source
